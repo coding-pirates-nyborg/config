4ff81595ee96219a0d49eade099aa89c0bd67e54
fclante <fclante@gmail.com>
2024-08-25 19:50:07 +0200

    UPDATE vscode config script

--- environment/linux-students/configure_vscode.sh
+++ environment/linux-students/configure_vscode.sh
@@ -4,13 +4,14 @@
 apt update &&  apt upgrade -y
 
 # Install required packages
-sudo apt-get install wget gpg
+sudo apt-get install -y software-properties-common apt-transport-https wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
-
 sudo rm -rf /var/lib/apt/lists/* && sudo apt-get update
+sudo apt install -y code
+
 # Install Visual Studio Code extensions
 code --install-extension donjayamanne.git-extension-pack \
     --install-extension mhutchie.git-graph \
