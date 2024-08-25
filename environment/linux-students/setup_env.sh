784b4cd85222ddc7452f66fb5c2e811e100d80a0
fclante <fclante@gmail.com>
2024-08-25 18:46:19 +0200

    UPDATE setup script for linux setup

--- environment/linux-students/setup_env.sh
+++ environment/linux-students/setup_env.sh
@@ -7,7 +7,7 @@ apt update &&  apt upgrade -y
 # Install required packages
 apt install -y curl wget git python3 python3-pip software-properties-common apt-transport-https \
     zip unzip build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev \
-    openssh-client
+    openssh-client vim nmap dnsutils mtr net-tools iputils-ping traceroute whois
 
 (type -p wget >/dev/null || (apt update &&  apt-get install wget -y)) \
 && mkdir -p -m 755 /etc/apt/keyrings \
@@ -16,6 +16,9 @@ apt install -y curl wget git python3 python3-pip software-properties-common apt-
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" |  tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 &&  apt update \
 &&  apt install gh -y
+
+sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main
+" > /etc/apt/sources.list.d/brave-browser-release.list
 
 . ./configure_git.sh
 . ./configure_vscode.sh
